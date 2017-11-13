6c19e533a7510fa51afd87e79b79b415ea69ca5f
Razvan Mocanu <razvan@cozmoslabs.com>
2017-11-13 09:26:57 +0100

    Added CSS. Removed unnecessary code.

--- js/fb_login.js
+++ js/fb_login.js
@@ -3,22 +3,20 @@
 
 // This is called with the results from from FB.getLoginStatus().
 function statusChangeCallback(response) {
-	console.log('statusChangeCallback');
-	console.log(response);
 	// The response object is returned with a status field that lets the
 	// app know the current login status of the person.
 	// Full docs on the response object can be found in the documentation
 	// for FB.getLoginStatus().
 	if (response.status === 'connected') {
 	  // Logged into your app and Facebook.
-	  testAPI();
+
 	  get_avatar(response.authResponse.userID);
 	  get_feed(response.authResponse.userID);
 
 	} else {
 	  // The person is not logged into your app or we are unable to tell.
-	  document.getElementById('status').innerHTML = 'Please log ' +
-	  'into this app.';
+	  document.getElementById('avatar_container').innerHTML = '';
+	  document.getElementById('feed_container').innerHTML = '';
 	}
 }
 
@@ -59,24 +57,14 @@ window.fbAsyncInit = function() {
 };
 
 // Load the SDK asynchronously
-(function(d, s, id) {
+
+(function(d, s, id) {	
 	var js, fjs = d.getElementsByTagName(s)[0];
 	if (d.getElementById(id)) return;
 	js = d.createElement(s); js.id = id;
 	js.src = "//connect.facebook.net/en_US/sdk.js";
 	fjs.parentNode.insertBefore(js, fjs);
+
 }(document, 'script', 'facebook-jssdk'));
 
-// Here we run a very simple test of the Graph API after login is
-// successful.  See statusChangeCallback() for when this call is made.
-function testAPI() {
-	console.log('Welcome!  Fetching your information.... ');
-	FB.api('/me', function(response) {
-		console.log('Successful login for: ' + response.name);
-		document.getElementById('status').innerHTML =
-		'Thanks for logging in, ' + response.name + '!';
-	});
-
-
-}
 
