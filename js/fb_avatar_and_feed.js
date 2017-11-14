30bba8f2ae44a42b5fd4a1b7c40ff001fb61a151
Razvan Mocanu <razvan@cozmoslabs.com>
2017-11-14 02:38:19 +0100

    Added restriction for displaying only post from last week.

--- js/fb_avatar_and_feed.js
+++ js/fb_avatar_and_feed.js
@@ -29,35 +29,36 @@ function get_feed(user_id){
 		return;
 	}
 	FB.api(
-	    "/"+ user_id + "/feed?fields=picture,message",
+	    "/"+ user_id + "/feed?fields=picture,message,created_time",
 	    function (response) {
 	    	if (response && !response.error) {
 	    		if ( response.data.length > 0 ){
-	  				console.log(response);
 	  				var feed_container = document.getElementById('feed_container');
 	  				var ul = document.createElement('ul');
 	  				ul.setAttribute('id', 'feed_list');
 	  				feed_container.appendChild(ul);
 	  				for ( var i in response.data ){
-						var li = document.createElement('li');
-						li.setAttribute('class', 'post');
-						if (response.data[i].picture){
-							var img = document.createElement('img');
-							img.setAttribute('src', response.data[i].picture );
-							img.setAttribute('class','float-left');
-							li.appendChild( img );		
-						}
+	  					if ( less_than_a_week_ago( response.data[i].created_time ) ){
+							var li = document.createElement('li');
+							li.setAttribute('class', 'post');
+							if (response.data[i].picture){
+								var img = document.createElement('img');
+								img.setAttribute('src', response.data[i].picture );
+								img.setAttribute('class','float-left');
+								li.appendChild( img );		
+							}
 
-						if ( response.data[i].message ){
-							var paragraph = document.createElement('p');
-							paragraph.innerHTML = response.data[i].message;
-							paragraph.setAttribute('class', 'message');
-							li.appendChild(paragraph);
+							if ( response.data[i].message ){
+								var paragraph = document.createElement('p');
+								paragraph.innerHTML = response.data[i].message;
+								paragraph.setAttribute('class', 'message');
+								li.appendChild(paragraph);
+							}
+							var div = document.createElement('div');
+							div.setAttribute('style','clear:both;')
+							li.appendChild(div);
+							ul.appendChild(li);
 						}
-						var div = document.createElement('div');
-						div.setAttribute('style','clear:both;')
-						li.appendChild(div);
-						ul.appendChild(li);
 					}
 	    		}
 	  		}
@@ -65,20 +66,9 @@ function get_feed(user_id){
 	);
 }
 
-function get_photo( photo_id ){
-	/* make the API call */
-	var img = document.createElement('img');
-	FB.api(
-	    "/" + photo_id,
-	    function (response) {
-	      		console.log(response);
-	      if (response && !response.error) {
-	      		//img.setAttribute('src', response.data.url );
-	        /* handle the result */
-	      }
-	    }
-	);
-
-	return img;
-
+function less_than_a_week_ago( created_date ){
+	var date = new Date(created_date);
+	var now = new Date();
+	var diff = Math.abs(now.getTime() - date.getTime());
+  	return diff / (1000 * 60 * 60 * 24) <= 7;
 }
