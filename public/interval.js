3edb25842c626b072a6574445b0ab953436f9f82
Razvan Mocanu <razvan@cozmoslabs.com>
2017-12-07 04:52:54 +0100

    Fixed JS error console when logged out. Check_interval() tried accessing html inputs that didn't exist

--- public/interval.js
+++ public/interval.js
@@ -1,13 +1,15 @@
 function check_interval( changed_input_id ){
 	var input = document.getElementById( changed_input_id );
-	var value = input.value;
+	if ( input ){
+		var value = input.value;
 
-	if ( changed_input_id == 'from_interval' ){
-		input = document.getElementById( 'to_interval' );
-		input.setAttribute( 'min', value );
-	}else if ( changed_input_id == 'to_interval' ) {
-		input = document.getElementById( 'from_interval' );
-		input.setAttribute( 'max', value );
+		if ( changed_input_id == 'from_interval' ){
+			input = document.getElementById( 'to_interval' );
+			input.setAttribute( 'min', value );
+		}else if ( changed_input_id == 'to_interval' ) {
+			input = document.getElementById( 'from_interval' );
+			input.setAttribute( 'max', value );
+		}
 	}
 }
 
